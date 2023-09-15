2b08a7cdc44c06959a6b3595a3980585c1e4fd6c
Saran <113179649+Santosaran@users.noreply.github.com>
2023-09-15 23:30:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const gameContainer = document.querySelector(".container"), 
   userResult = document.querySelector(".user_result img"),
-  cpuResult = document.querySelector(".cpu_result img"),
+  cpuResult = document.querySelector(".cpu_result img"), 
   result = document.querySelector(".result"),
   optionImages = document.querySelectorAll(".option_image");
 // Loop through each option image element
